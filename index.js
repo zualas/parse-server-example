9c70268c915892304763059722106005be123f2c
Dmitri Borohhov <hello@ausalt.com>
2016-06-07 12:04:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
   emailAdapter: {
     module: 'parse-server-simple-mailgun-adapter',
     options: {
